efd989d89d1f909e1223ee81f653d3c3fc3179e8
BenitoBachmeyer <benitobachmeyer@gmail.com>
2026-02-08 12:02:03 +0100

    code review fixes

--- internal/abgabe/abgabe.go
+++ internal/abgabe/abgabe.go
@@ -19,14 +19,13 @@ const (
 
 // Abgabe Model this contains all information about the regular "tasks"
 // belongs to one user only and one user can have many tasks
-// @author Benito Bachmeyer
 type Abgabe struct {
 	gorm.Model
-	Title         string `json:"title"`
-	DueDate       time.Time
-	RiskAssesment Risk             `json:"risk_assesment"`
-	UserID        uint             `gorm:"not null; index"`
-	ModulID       uint             `gorm:"not null; index"`
-	User          models.User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Modul         UniversityModule `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	Title          string           `json:"title"`
+	DueDate        time.Time        `json:"due_date"`
+	RiskAssessment Risk             `json:"risk_assessment"`
+	UserID         uint             `gorm:"not null;index"`
+	ModulID        uint             `gorm:"not null;index"`
+	User           models.User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Modul          UniversityModule `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
 }
--- internal/abgabe/university_module.go
+++ internal/abgabe/university_module.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 // This Struct contains information regarding university modules
 // Abgaben can be linked to this module (one to many)
-// @author Benito Bachmeyer
 type UniversityModule struct {
 	gorm.Model
 	Name string `json:"name"`
