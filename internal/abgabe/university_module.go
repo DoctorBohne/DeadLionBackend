28e976ca385583cef46480212b759c2bfb80c563
BenitoBachmeyer <benitobachmeyer@gmail.com>
2026-02-08 11:52:18 +0100

    added docs

--- internal/abgabe/university_module.go
+++ internal/abgabe/university_module.go
@@ -2,6 +2,9 @@ package abgabe
 
 import "gorm.io/gorm"
 
+// This Struct contains information regarding university modules
+// Abgaben can be linked to this module (one to many)
+// @author Benito Bachmeyer
 type UniversityModule struct {
 	gorm.Model
 	Name string `json:"name"`
