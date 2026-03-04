d9e3bad71a9b20890373b444cc346062ec664df0
doctorbohne <samuel.naumann2006@gmail.com>
2026-03-04 21:19:20 +0100

    gin context got now issuer and sub

--- internal/http/middleware/gin_middleware.go
+++ internal/http/middleware/gin_middleware.go
@@ -40,7 +40,8 @@ func AuthMiddleware(v *auth.Verifier) gin.HandlerFunc {
 		}
 		ctx := requestctx.WithClaims(c.Request.Context(), rc)
 		c.Request = c.Request.WithContext(ctx)
-
+		c.Set("issuer", claims.Issuer)
+		c.Set("sub", claims.Subject)
 		c.Set(GinClaimsKey, rc)
 		c.Next()
 	}
