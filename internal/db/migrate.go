b6d691972c77971315256d829f67a6749e54cc4c
BenitoBachmeyer <benitobachmeyer@gmail.com>
2026-03-18 15:42:57 +0100

    bugfix hml 3

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -14,7 +14,6 @@ func Migrate(gdb *gorm.DB) error {
 	if err := gdb.AutoMigrate(
 		&models.User{},
 		&abgabe.Abgabe{},
-		&abgabe.UniversityModule{},
 		&models.Userboard{},
 		&models.BoardPool{},
 		&models.Task{},
@@ -22,6 +21,16 @@ func Migrate(gdb *gorm.DB) error {
 		&models.Taskboard{},
 	); err != nil {
 		return err
+	}
+	if gdb.Migrator().HasColumn(&abgabe.Abgabe{}, "modul_id") {
+		if err := gdb.Migrator().DropColumn(&abgabe.Abgabe{}, "modul_id"); err != nil {
+			return err
+		}
+	}
+	if gdb.Migrator().HasTable("university_modules") {
+		if err := gdb.Migrator().DropTable("university_modules"); err != nil {
+			return err
+		}
 	}
 	return nil
 }
